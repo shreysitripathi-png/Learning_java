f0d006e6947a6fc3cef56e120c688ac5ec306773
Shreysi Tripathi <shreysitripathi@gmail.com>
2026-09-11 11:39:21 +0530

    useCaseTwo

---
+++ useCaseTwo.java
@@ -0,0 +1,48 @@
+abstract class Shape{
+    abstract double area();
+
+    void displayArea(){
+        System.out.println("Area = " + area());
+    }
+}
+
+class Circle extends Shape{
+
+    private double radius;
+
+    Circle(double radius){
+        this.radius = radius;
+    }
+
+    @Override 
+    double area(){
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape{
+    private double length;
+    private double breadth;
+
+    public Rectangle(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    @Override 
+    double area(){
+        return length*breadth;
+    }
+
+}
+public class useCaseTwo {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
+ 
+        for (Shape s : shapes) {
+            s.displayArea();  
+        }
+    }
+}
